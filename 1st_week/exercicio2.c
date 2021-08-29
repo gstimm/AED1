2b92fdb4364db55bd888649bb66eda740420aeeb
gstimm <contato.gabrieltimm@gmail.com>
2021-08-29 17:18:56 -0300

    feat: ex1 done, ex2 working with warning

---
+++ 1st_week/exercicio2.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NAME_SIZE 10
+
+int menu(void);
+char *insertName(char *namesList);
+char *removeName(char *namesList);
+void listNames(char *namesList);
+
+int main() {
+  int choice;
+  char *namesList = NULL;
+
+  namesList = (char *)malloc(sizeof(char) + 1);
+  namesList[0] = '\0';
+
+  if (namesList == NULL) {
+    printf("Memória insuficiente.\n");
+    exit(1);
+  }
+
+  while (1) {
+    choice = menu();
+    switch (choice) {
+    case 1:
+      namesList = insertName(namesList);
+      break;
+    case 2:
+      namesList = removeName(namesList);
+      break;
+    case 3:
+      listNames(namesList);
+      break;
+    case 4:
+      free(namesList);
+      exit(0);
+      break;
+    }
+  }
+
+  return 0;
+}
+
+int menu(void) {
+  int choice = 0;
+  do {
+    printf("MENU:\n");
+    printf("- 1. Inserir um nome\n");
+    printf("- 2. Excluir um nome\n");
+    printf("- 3. Listar os nomes\n");
+    printf("- 4. Sair\n");
+    printf("Digite sua escolha: ");
+    scanf("%d", &choice);
+  } while (choice < 1 || choice > 4);
+  getchar();
+  return choice;
+}
+
+char *insertName(char *namesList) {
+  char newName[NAME_SIZE];
+
+  printf("Digite um nome: ");
+  scanf("%s", newName);
+
+  printf("%s\n", namesList);
+
+  if (strlen(namesList) == 0) {
+    namesList = (char *)realloc(namesList, (strlen(newName) + 1) * sizeof(char));
+    strcat(namesList, newName);
+  } else {
+    namesList = (char *)realloc(namesList, (strlen(namesList) + 2 + strlen(newName)) * sizeof(char));
+    strcat(namesList, ",");
+    strcat(namesList, newName);
+  }
+
+  strcat(namesList, "\0");
+
+  return namesList;
+}
+
+char *removeName(char *namesList) {
+  char nameToRemove[NAME_SIZE];
+  int auxIndex, firstCharOfNameIndex;
+
+  printf("Digite um nome: ");
+  scanf("%s", nameToRemove);
+
+  char *nameIndex = strstr(namesList, nameToRemove);
+
+  if (nameIndex == NULL) {
+    printf("Nome não encontrado.\n");
+    return namesList;
+  }
+
+  firstCharOfNameIndex = nameIndex - namesList;
+
+  auxIndex = firstCharOfNameIndex + strlen(nameToRemove);
+  if (strstr(nameIndex, ",")) {
+    auxIndex++;
+  }
+
+  if (strchr(namesList, ',')) {
+    while (namesList[firstCharOfNameIndex] != '\0') {
+      namesList[firstCharOfNameIndex] = namesList[auxIndex];
+      auxIndex++;
+      firstCharOfNameIndex++;
+    }
+  } else {
+    strcpy(namesList, "\0");
+  }
+
+  if (strlen(namesList) > 0 && namesList[strlen(namesList) - 1] == ',') {
+    namesList[strlen(namesList) - 1] = '\0';
+  }
+
+  namesList = (char *)realloc(namesList, (strlen(namesList) + 1) * sizeof(char));
+
+  strcat(namesList, "\0");
+
+  return namesList;
+}
+
+void listNames(char *namesList) {
+  if (strlen(namesList) == 0) {
+    printf("Lista vazia :(\n");
+  } else {
+    printf("Nomes: %s\n", namesList);
+  }
+}
